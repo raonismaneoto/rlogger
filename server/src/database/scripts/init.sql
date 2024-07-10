6a7905fb12d47a65fa34a8326dd17be3ed26c952
raonismaneoto <raoni.smaneoto@gmail.com>
2024-07-09 21:13:33 -0300

    chore: server continuation

--- server/src/database/scripts/init.sql
+++ server/src/database/scripts/init.sql
@@ -1,8 +1,12 @@
-create table log(
+create schema default_idx;
+
+set search_path to default_idx;
+
+create table default_idx.log(
     id serial primary key,
     log_type varchar(255),
     log_message varchar(255),
     log_timestamp timestamp default current_timestamp,
-    log_attributes json[],
-    log_index varchar(255)
-)
+    log_attributes jsonb[]
+);
+
--- server/src/log/repo.rs
+++ server/src/log/repo.rs
@@ -1,6 +1,6 @@
 use crate::{database::storage::Storage, error::app_error::DynAppError};
 
-use super::log::Log;
+use super::log::{Log, LogAttribute};
 
 #[derive(Clone)]
 pub struct LogRepo {
@@ -13,8 +13,22 @@ impl LogRepo {
     }
     
     pub async fn save_log(&self, log: Log) -> Result<u64, DynAppError> {
-        let cmd = String::from("");
+        let cmd = String::from(
+            "insert into 
+                $1.log (log_type, log_message, log_timestamp, log_attributes)
+            values
+                ($2, $3, $4, $5, $6);
+            "
+        );
 
-        self.storage.exec(cmd, &[]).await
+        self.storage.exec(cmd, &[&log.index, &log.message, &log.timestamp, &log.attributes]).await
+    }
+
+    fn parse_log_attributes(attributes : Option<Vec<LogAttribute>>) -> String {
+        if let Some(attrs) = attributes {
+            
+        }
+
+        String::from("ARRAY[]::json")
     }
 }
